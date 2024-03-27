60138a221d3b1573cae3ee7bfa8fbf5107650f4e
IBRA <ifanl.ibrahim@gmail.com>
2024-03-28 00:54:49 +0100

    bug déploiment sur l'importation des fichier css de la navbar et du footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
-import styles from '../styles/footer.module.css';
+import styles from '/styles/footer.module.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
--- components/NavBar.js
+++ components/NavBar.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Link from 'next/link';
-import styles from '../styles/navBar.module.css';
+import styles from '/styles/navBar.module.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
