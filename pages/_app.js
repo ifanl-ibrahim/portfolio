c6e9c3e03f4a6e1cd33a343642458defa8b5e417
IBRA <ifanl.ibrahim@gmail.com>
2024-03-28 00:43:19 +0100

    bug déploiment sur l'importation des fichier css de la navbar et du footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import '../styles/globals.css'
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
 
 i18next.use(initReactI18next).use(Backend).init({
     backend: {
@@ -16,9 +14,7 @@ i18next.use(initReactI18next).use(Backend).init({
 function MyApp({ Component, pageProps }) {
     return (
         <div>
-            <NavBar />
             <Component {...pageProps} />
-            <Footer />
         </div>
     )
 }
