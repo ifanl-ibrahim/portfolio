fad05304b73659207ca644bf3622989cf8f71ca8
IBRA <ifanl.ibrahim@gmail.com>
2024-03-28 01:07:56 +0100

    modif fichier next.config pour bug déploiment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.node = {
+                fs: 'empty'
+            }
+        }
+        return config
+    },
+    cssModules: true,
+    cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: "[local]___[hash:base64:5]",
+    },
+};
 
 export default nextConfig;
